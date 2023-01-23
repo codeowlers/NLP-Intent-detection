cdfa7c9f55c57daa5022f56cb3a64e61664a19ec
Hadi Ibrahim <17hadiibrahim@gmail.com>
2023-01-23 21:51:34 +0100

    feat(feature-selection): spread the columns of the feature extracted by librosa
    BREAKING CHANGE:

--- algorithms/array_column_spread.py
+++ algorithms/array_column_spread.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-def array_column_spread(dataframe, column):
+def array_column_spread(df, column_name):
     df[['{}_{}'.format(column_name, i) for i in range(len(df[column_name].iloc[0]))]] = df[column_name].apply(lambda x: pd.Series(x))
     df.drop(columns=[column_name], inplace=True)
