e28784bd10919de4da7095129840f919112875ae
developer-sidani <developer.sidani@gmail.com>
2023-01-24 19:47:12 +0100

    feat: testing on the encoded for the dataset and the evaluation

--- algorithms/svm_model.py
+++ algorithms/svm_model.py
@@ -1,8 +1,9 @@
 from sklearn.svm import SVC
 
+# [(0.1, 'linear', 0.1), (0.1, 'linear', 1), (0.1, 'poly', 0.1), (0.1, 'poly', 1), (0.1, 'rbf', 0.1), (0.1, 'rbf', 1), (1, 'linear', 0.1), (1, 'linear', 1), (1, 'poly', 0.1), (1, 'poly', 1), (1, 'rbf', 0.1), (1, 'rbf', 1), (10, 'linear', 0.1), (10, 'linear', 1), (10, 'poly', 0.1), (10, 'poly', 1), (10, 'rbf', 0.1), (10, 'rbf', 1)]
 
 def svm_model(X_train, y_train, X_test):
-    clf = SVC()
+    clf = SVC(C=0.1, kernel='linear', gamma=0.1)
     # train the model on the training data
     clf.fit(X_train, y_train)
     # predict the target values for the test data
